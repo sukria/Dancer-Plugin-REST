8f0efe4193c2531a2ad9ce3c2aafe7e619505899
Alexis Sukrieh <sukria@sukria.net>
2010-04-03 17:24:42 +0200

    TestUtils support body params in fake_request

--- t/TestUtils.pm
+++ t/TestUtils.pm
@@ -10,14 +10,18 @@ use Dancer::Config 'setting';
 @EXPORT =
   qw(fake_request get_response_for_request);
 
-sub fake_request($$) {
-    my ($method, $path) = @_;
-    return Dancer::Request->new_for_request($method => $path);
+sub fake_request($$;$) {
+    my ($method, $path, $params) = @_;
+    my $req = Dancer::Request->new_for_request($method => $path);
+    if ($params) {
+       $req->_set_body_params($params); 
+    }
+    return $req;
 }
 
 sub get_response_for_request {
-    my ($method, $path) = @_;
-    my $request = fake_request($method => $path);
+    my ($method, $path, $params) = @_;
+    my $request = fake_request($method => $path, $params);
     Dancer::SharedData->request($request);
     Dancer::Renderer::get_action_response();
 }
